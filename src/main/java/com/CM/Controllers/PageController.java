69fe869ed5cf32854b5a33ddc78583e71f48edd9
Sabyasachi Mohapatra <mohapatrasabyasachi162004@gmail.com>
2024-10-11 10:52:56 +0530

    add page controller

--- src/main/java/com/CM/Controllers/PageController.java
+++ src/main/java/com/CM/Controllers/PageController.java
@@ -7,6 +7,47 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PageController {
-       
+    @RequestMapping("/")
+    public String index() {
+        return "redirect:/home";
+    }
+
+    @RequestMapping("/home")
+public String home() {
+    return "home";
+}
+
+// about route
+
+@RequestMapping("/about")
+public String aboutPage() {
+    return "about";
+}
+
+// services
+
+@RequestMapping("/services")
+public String servicesPage() {
+    return "services";
+}
+
+// contact page
+
+@GetMapping("/contact")
+public String contact() {
+    return new String("contact");
+}
+
+// this is showing login page
+@GetMapping("/login")
+public String login() {
+    return new String("login");
+}
+
+// registration page
+@GetMapping("/register")
+public String register() {
+    return "register";
+}
 
 }
